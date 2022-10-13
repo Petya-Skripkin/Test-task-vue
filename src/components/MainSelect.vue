748bc1363c78f63318e541baf86ab2a0f267f71b
Petya-Skripkin <sk.petya02@mail.ru>
2022-10-13 12:21:03 +0600

    Skeleton version

---
+++ src/components/MainSelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="select" :class="isCard && 'cart'">
+    <slot></slot>
+    <span v-if="count !== undefined && maxCount" class="selected">
+      selected: {{count}} / {{maxCount}}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainSelect',
+  props: ['count', 'maxCount', 'isCard']
+}
+</script>
+
+<style scoped>
+.select {
+  flex: 1;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  /* justify-content: flex-start;
+    flex-wrap: wrap; */
+  gap: 10px;
+  padding: 10px 15px;
+  border: 1.5px solid #6a6a6a;
+  border-radius: 5px;
+}
+
+.select.cart {
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+.selected {
+  width: 100%;
+  display: inline-block;
+  text-align: center;
+  align-self: flex-end;
+  justify-self: center;
+  margin: 10px 0;
+}
+</style>
+
---
+++ src/components/ProductItem.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="item" :class="active && 'active' || isCard && 'card'" @click="ClickHandler">
+    {{data.name}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductItem',
+  props: ['active','data', 'isCard'],
+  methods: {
+    ClickHandler() {
+      this.$emit('getItem', this.data.id)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.item {
+  /* width: calc(100% / 5 - 10px); */
+  padding: 10px 15px;
+  border: 1.5px solid #00b419;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.item.card {
+  width: calc(100% / 6 - 10px);
+}
+
+.item.active {
+  border-color: rgb(255, 183, 0);
+}
+</style>
